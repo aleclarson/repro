bdc165c3aa3ca838e032bc47a9af9efc6ba1653b
Philipp Schiffmann <philippschiffmann93@gmail.com>
2019-10-11 13:19:52 +0200

    Add reproduction code

---
+++ src/main.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+const DEFAULT_MESSAGE = "loading ...";
+
+const MyComponent: React.FC<{ level: number; color: string }> = ({
+  level,
+  color
+}) => {
+  const [message, setMessage] = useState(DEFAULT_MESSAGE);
+  useEffect(() => {
+    setMessage(`Level ${level}`);
+  });
+
+  return (
+    <div style={{ border: `4px solid ${color}` }}>
+      {message}
+      {message !== DEFAULT_MESSAGE && level > 0 && (
+        <MyComponent level={level - 1} color={color} />
+      )}
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <MyComponent level={10} color="green" />,
+  document.querySelector("#root1")
+);
+ReactDOM.render(
+  <MyComponent level={10} color="red" />,
+  document.querySelector("#root2")
+);
+ReactDOM.render(
+  <MyComponent level={10} color="yellow" />,
+  document.querySelector("#root3")
+);
