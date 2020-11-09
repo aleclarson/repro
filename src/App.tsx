0ebb4e9f39df3e179d58c6591012a72bafe497cd
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-11-09 08:45:25 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+function App(): JSX.Element {
+  return (
+    <Container>Hello world</Container>
+  );
+}
+
+export default App;
