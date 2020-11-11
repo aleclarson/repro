9ca49d04769be81fd8696e8da217052a36d86c6e
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-11-11 11:04:51 -0500

    fix: use full-featured systemjs
    For UMD support

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import type { UserConfig } from 'vite'
 const config: UserConfig = {
   jsx: 'react',
   plugins: [reactPlugin],
+  minify: false,
   sourcemap: true,
-  useSystemJs: true,
-  // useSystemJs: 'https://unpkg.com/systemjs@6.7.1/dist/s.min.js',
+  // useSystemJs: true,
+  useSystemJs: 'https://unpkg.com/systemjs@6.7.1/dist/system.min.js',
   rollupInputOptions: {
     external: ['react', 'react-dom'],
   }
