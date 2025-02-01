bc99f0a41d2d16af675ba5929a3c0784d604b1b5
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-02-01 11:16:12 -0500

    reproduce the bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,112 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
+import * as culori from "culori";
+import "./App.css";
+
+const randomColor = (hue: [number, number] = [0, 360]) =>
+  culori.formatHsl(
+    culori.random("hsl", { h: hue, s: [0.33, 0.4], l: [0.55, 0.6] })
+  );
+
+const shortPoems = [
+  "A silly cat named Fred, danced on his head",
+  "A rubber duck quacks wise, but only tells me lies",
+  "The dishes in my sink, started to wink",
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState(() =>
+    Array.from({ length: 3 }, (_, i) => ({
+      color: randomColor([i * 40, i * 45 + 20]),
+      poem: shortPoems[i],
+      key: i,
+    }))
+  );
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        <strong>Goal:</strong> A poem should only be visible when it is at the
+        top of the list.
       </p>
+      <button
+        style={{
+          marginBottom: "2rem",
+        }}
+        onClick={() => {
+          setItems((items) => {
+            items = [...items];
+            items.push(items.shift()!);
+            return items;
+          });
+        }}
+      >
+        Move top item to bottom
+      </button>
+
+      {items.map(({ color, poem, key }, i) => (
+        <ListItem key={key} color={color} poem={poem} isOpen={i === 0} />
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+function ListItem({
+  color,
+  poem,
+  isOpen,
+}: {
+  color: string;
+  poem: string;
+  isOpen: boolean;
+}) {
+  console.log("[ListItem] render", { isOpen, poem });
+
+  const transition = {
+    opacity: { duration: 0.2 },
+    height: { type: "spring", mass: 0.8 },
+  };
+
+  return (
+    <div style={{ backgroundColor: color }}>
+      <AnimatePresence mode="wait" initial={false}>
+        {isOpen ? (
+          <motion.div
+            key="content"
+            initial="hide"
+            animate="show"
+            exit="hide"
+            variants={{
+              show: {
+                opacity: 1,
+                height: "auto",
+                transition,
+              },
+              hide: {
+                opacity: 0,
+                height: "3rem",
+                transition,
+              },
+            }}
+          >
+            <div
+              style={{
+                height: "5rem",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                color: "white",
+              }}
+            >
+              {poem}
+            </div>
+          </motion.div>
+        ) : (
+          <div key="spacer" style={{ height: "3rem" }} />
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+let app = <App />;
+
+// Comment this out to see it work as intended.
+app = <StrictMode>{app}</StrictMode>;
+
+createRoot(document.getElementById("root")!).render(app);
